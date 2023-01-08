6bb90a327b38923269401170024fb896eb599b59
josu_shell <yoy07030@khu.ac.kr>
2023-01-08 20:48:09 +0900

    FEAT: 나의 서재 탭 완성

---
+++ MyLib/MyPageTab.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageTab.swift
+//  BookMark
+//
+//  Created by JOSUEYEON on 2023/01/08.
+//
+
+import UIKit
+
+// MARK: - 마이 페이지 탭
+class MyPageTab: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
