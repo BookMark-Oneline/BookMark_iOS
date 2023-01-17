623ba3009def657cc49973ed9cd924e7191092f8
josu_shell <yoy07030@khu.ac.kr>
2023-01-17 14:18:33 +0900

    FEAT: 서버 API 연결

--- MyLib/Network.swift
+++ MyLib/Network.swift
@@ -1,5 +1,5 @@
 //
-//  Networking.swift
+//  Network.swift
 //  BookMark
 //
 //  Created by JOSUEYEON on 2023/01/15.
@@ -8,6 +8,77 @@
 import Foundation
 import Alamofire
 
-class Network {
-    
+// MARK: - 네트워킹 result 열거형
+enum NetworkResult<T> {
+    case success(T)
+    case requestErr(T)
+    case pathErr
+    case serverErr
+    case networkFail
 }
+
+// MARK: - 책 세부내용 data class
+class BookDetail: Decodable {
+    let user_id: Int
+    let title: String
+    let author: String
+    let img_url: String
+    let publisher: String
+    let ave_reading_time: Int
+    let ave_reading_page: Int
+}
+
+// MARK: - 네트워킹 용 클래스 나중에 싱글톤으로 만들기
+class Network {
+    // base Url
+    let baseUrl = ""
+    
+    // 책 등록 POST
+    func registerBooks(completion: @escaping (() -> Void)) {
+        let params: Parameters = ["title": "Do it! 자료구조와 함께 배우는 알고리즘 입문 : 파이썬 편", "img_url": "http://image.yes24.com/goods/91219874/XL", "author": "시바타 보요", "publisher": "이지스퍼블리싱", "isbn": "9791163031727"]
+        
+        let datarequest = AF.request("url", method: .post, parameters: params, encoding: JSONEncoding.default).validate()
+        
+        datarequest.responseData(completionHandler: { response in
+            switch response.result {
+            case .success:
+                print("ok")
+            case .failure(let e):
+                print(e)
+            }
+            //MARK: todo - completion에 NetworkResult<AnyObject>으로 추가하기 나중에
+            completion()
+        })
+    }
+    
+    // 책 세부내용 조회 GET
+    func getBookDetail(completion: @escaping (NetworkResult<AnyObject>) -> Void) {
+        let header: HTTPHeaders = ["값": "값"]
+        let datarequest = AF.request("url", method: .get, encoding: JSONEncoding.default, headers: header)
+        
+        datarequest.responseData(completionHandler: { res in
+            switch res.result {
+            case .success:
+                print("ok")
+                guard let value = res.value else {return}
+                let bookDetailData = self.decodeJSON(data: value)
+                print(bookDetailData)
+                
+            case .failure(let e):
+                print(e)
+            }
+        })
+    }
+    
+    
+    // JSON 객체 Decode
+    func decodeJSON(data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decoding = try? decoder.decode(BookDetail.self, from: data) else {
+            return .networkFail
+        }
+        return .success(decoding)
+        
+    }
+}
+
