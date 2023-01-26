7f085a988b90e4b49f601c066275da350d249cb1
josu_shell <yoy07030@khu.ac.kr>
2023-01-26 17:23:40 +0900

    FEAT: 모임 생성/설정 view 완성 및 이벤트 추가

---
+++ BookCommunitySettings/CreateCommunityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateCommunityViewController.swift
+//  BookMark
+//
+//  Created by JOSUEYEON on 2023/01/26.
+//
+
+import UIKit
+
+class CreateCommunityViewController: CommunitySettingBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookCommunitySettings/SetCommunityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SetCommunityViewController.swift
+//  BookMark
+//
+//  Created by JOSUEYEON on 2023/01/26.
+//
+
+import UIKit
+import SnapKit
+
+// MARK: - 모임 설정 view controller
+class SetCommunityViewController: CommunitySettingBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
