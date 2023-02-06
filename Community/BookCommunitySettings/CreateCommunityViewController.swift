b7400ff4dbc50e6c9967631f83e7687ca60007a2
josu_shell <yoy07030@khu.ac.kr>
2023-02-06 21:57:59 +0900

    FIX #51 : 화면 전환 속도 개선

--- Community/BookCommunitySettings/CreateCommunityViewController.swift
+++ Community/BookCommunitySettings/CreateCommunityViewController.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 // MARK: - 모임 생성 view controller
-class CreateCommunityViewController: CommunitySettingBaseViewController {
+class CreateCommunityViewController: UIViewController {
+    let layout_SetCommunity = CommunitySettingView()
+    let imgPicker = UIImagePickerController()
     let label_title = UILabel()
     let layout_main = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         setBaseView()
         setNavCustom()
+        setImgPicker()
     }
 
     private func setBaseView() {
@@ -41,12 +45,41 @@ class CreateCommunityViewController: CommunitySettingBaseViewController {
     }
     
     @objc func goBackCommunityTab(_ sender: UIBarButtonItem) {
-        print("ok")
-        
         if let appdel = UIApplication.shared.delegate as? AppDelegate {
             appdel.communities.append(["myeongsoo", "책과 무스비"])
         }
         
         self.navigationController?.popToRootViewController(animated: true)
+    }
+}
+
+
+// MARK: - 이미지 피커 컨트롤러 extension
+extension CreateCommunityViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    private func setImgPicker() {
+        imgPicker.sourceType = .photoLibrary
+        imgPicker.allowsEditing = true
+        imgPicker.delegate = self
+    }
+    
+    @objc func imagePicker(_ sender: UITapGestureRecognizer) {
+        present(self.imgPicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var newImage: UIImage? = nil
+        
+        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage = possibleImage
+        }
+        
+        self.layout_SetCommunity.layout_img.image = newImage
+        picker.dismiss(animated: true, completion: {
+            self.layout_SetCommunity.img_iconIMG.isHighlighted = true
+        })
     }
 }
